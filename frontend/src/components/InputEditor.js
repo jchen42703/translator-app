f1f31cc5a86c68ad8b7fc9fa55fbab331cba03fb
jchen42703 <jchen42703@gmail.com>
2021-03-18 22:11:05 -0400

    InputEditor is now a parent component of TranslatedDisplay

--- frontend/src/components/InputEditor.js
+++ frontend/src/components/InputEditor.js
@@ -1,39 +1,21 @@
 import React from "react";
 import { EditorState, Editor } from "draft-js";
 import "draft-js/dist/Draft.css";
-import debounce from "lodash/debounce";
+import TranslatedDisplay from "./TranslatedDisplay.js";
 
 class InputEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      toText: "",
+    };
   }
 
-  saveContent = debounce((content) => {
-    fetch("/translate/", {
-      method: "POST",
-      body: JSON.stringify({
-        foreign: content,
-      }),
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-    });
-  }, 1000);
-
   onChange = (editorState) => {
-    const contentState = editorState.getCurrentContent();
-    this.saveContent(contentState.getPlainText());
     this.setState({
-      editorState,
-    });
-  };
-
-  onChange = (editorState) => {
-    const contentState = editorState.getCurrentContent();
-    console.log("Content state text: " + contentState.getPlainText());
-    this.setState({
-      editorState,
+      editorState: editorState,
+      toText: editorState.getCurrentContent().getPlainText(),
     });
   };
 
@@ -41,6 +23,7 @@ class InputEditor extends React.Component {
     return (
       <div>
         <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <TranslatedDisplay text={this.state.toText}></TranslatedDisplay>
       </div>
     );
   }
