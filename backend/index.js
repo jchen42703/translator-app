c20a9259c2110be08eb3b251395048cb5fbd7574
jchen42703 <jchen42703@gmail.com>
2021-03-18 22:09:36 -0400

    Added some testing comments

--- backend/index.js
+++ backend/index.js
@@ -6,25 +6,14 @@ const { translate } = require("google-translate-api-browser");
 app.use(bodyParser.json());
 
 app.post("/translate/", async (req, res) => {
+  // console.log(
+  //   `Foreign: ${req.body.foreign}, typeof: ${typeof req.body.foreign}`
+  // );
   let translatedText = await translate(req.body.foreign, { to: "en" });
-  // .then((res) => {
-  //   console.log(`Translated: ${res.text}`);
-  // })
-  // .catch((err) => {
-  //   console.error(err);
-  // });
-  console.log("translated text: " + translatedText);
+  // console.log("translated text: " + translatedText.text);
   res.send({ translated: translatedText.text });
   // res.end("Finished translating");
 });
-
-// app.post("/content", (req, res) => {
-//   var out = res.json();
-//   var str = JSON.stringify(out, null, 2); // spacing level = 2
-
-//   console.log(`Res: ${str}`);
-//   res.close();
-// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
