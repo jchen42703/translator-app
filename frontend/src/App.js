193780270198bc1842804ccc35f5d984270a792a
jchen42703 <jchen42703@gmail.com>
2021-04-29 12:09:51 -0400

    Replaced inline styling with className="center-basic"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import Translator from "./containers/Translator";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import "./styles/css/abstracts/global.css";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
-        <h1 style={{ "text-align": "center" }}>
-          Basic UI for Translation (Dev)
-        </h1>
+        <h1 className="center-basic">Basic UI for Translation (Dev)</h1>
         <Translator></Translator>
       </div>
     </Provider>
