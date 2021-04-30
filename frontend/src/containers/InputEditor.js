bdbe02ab5a0e07061d22086705fbbecae01221d9
jchen42703 <jchen42703@gmail.com>
2021-04-30 02:44:06 -0400

    Refactored to a hook that uses props.editorState as the display and updates the parent state
    Much cleaner

--- frontend/src/containers/InputEditor.js
+++ frontend/src/containers/InputEditor.js
@@ -1,35 +1,14 @@
 import React from "react";
-import { EditorState, Editor } from "draft-js";
-import { connect } from "react-redux";
-import { toTextUpdate } from "../redux/toText.actions";
+import { Editor } from "draft-js";
 import "../styles/css/components/InputEditor.css";
 
-class InputEditor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editorState: EditorState.createEmpty(),
-      toText: "",
-    };
-  }
+const InputEditor = (props) => {
+  return (
+    <div className="InputEditor">
+      <h2>Type below:</h2>
+      <Editor editorState={props.editorState} onChange={props.setEditorState} />
+    </div>
+  );
+};
 
-  onChange = (editorState) => {
-    this.setState({
-      editorState: editorState,
-      toText: editorState.getCurrentContent().getPlainText(),
-    });
-    // dispatches the updated state to the store
-    this.props.dispatch(toTextUpdate(this.state.toText));
-  };
-
-  render() {
-    return (
-      <div className="InputEditor">
-        <h2>Type below:</h2>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
-      </div>
-    );
-  }
-}
-
-export default connect()(InputEditor);
+export default InputEditor;
