4c3cad7b48a4b13a80094c5b99c76b9b446fee95
jchen42703 <jchen42703@gmail.com>
2021-04-30 02:10:57 -0400

    Basic Line Number for InputEditor using EditorBlock
    credits to https://gist.github.com/schabluk/0e5e938bc30a7833201b605cee4efb4a

--- frontend/src/containers/InputEditor.js
+++ frontend/src/containers/InputEditor.js
@@ -2,7 +2,12 @@ import React from "react";
 import { EditorState, Editor } from "draft-js";
 import { connect } from "react-redux";
 import { toTextUpdate } from "../redux/toText.actions";
+import { Line } from "./Line.js";
 import "../styles/css/components/InputEditor.css";
+
+const blockRendererFn = () => ({
+  component: Line,
+});
 
 class InputEditor extends React.Component {
   constructor(props) {
@@ -26,7 +31,11 @@ class InputEditor extends React.Component {
     return (
       <div className="InputEditor">
         <h2>Type below:</h2>
-        <Editor editorState={this.state.editorState} onChange={this.onChange} />
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          blockRendererFn={blockRendererFn}
+        />
       </div>
     );
   }
