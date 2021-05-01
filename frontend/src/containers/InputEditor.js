42c50b9fa2ee4e13cd89619ca50a501b4c8223a8
jchen42703 <jchen42703@gmail.com>
2021-05-01 11:04:56 -0400

    Rendering the LineGutter component with InputEditor

--- frontend/src/containers/InputEditor.js
+++ frontend/src/containers/InputEditor.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Editor } from "draft-js";
+import { LineGutter } from "./LineGutter";
 import "../styles/css/components/InputEditor.css";
+
+const blockRendererFn = () => ({
+  component: LineGutter,
+});
 
 const InputEditor = (props) => {
   return (
     <div className="InputEditor">
       <h2>Type below:</h2>
-      <Editor editorState={props.editorState} onChange={props.setEditorState} />
+      <Editor
+        editorState={props.editorState}
+        onChange={props.setEditorState}
+        blockRendererFn={blockRendererFn}
+      />
     </div>
   );
 };
