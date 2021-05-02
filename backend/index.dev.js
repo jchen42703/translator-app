82862dc3dae909ff041312ab1d2fb22f17d7c288
jchen42703 <jchen42703@gmail.com>
2021-05-02 12:12:12 -0400

    Created index.dev.js as per #23

---
+++ backend/index.dev.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { translate } = require("google-translate-api-browser");
+const translateWord = require("./src/translate");
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/translate/", async (req, res) => {
+  switch (req.body.devMode) {
+    case "regular":
+      // console.log(
+      //   `Foreign: ${req.body.foreign}, typeof: ${typeof req.body.foreign}`
+      // );
+      let translatedText = await translate(req.body.foreign, { to: "en" });
+      console.log("translated text: " + translatedText.text);
+      res.send({ translated: translatedText.text });
+      break;
+    // longer output that preserves new lines by prepending the text to the 1st paragraph
+    case "pre":
+      console.log("You are using the dev api...");
+      const noLineBreaksStr = req.body.foreign.replace(/\r?\n|\r/g, "");
+      // console.log(
+      //   `Foreign: ${req.body.foreign}, typeof: ${typeof req.body.foreign}`
+      // );
+      const sendJson = {
+        translated: noLineBreaksStr + noLineBreaksStr + req.body.foreign,
+      };
+      console.log(`Sending json.translated ${sendJson.translated}`);
+      res.send(sendJson);
+    default:
+      // fail with no devMode
+      res
+        .status(400)
+        .send("Please specify a correct devMode in the POST request.");
+  }
+});
+
+// return translated word
+app.post("/translateWord/", (req, res) => {
+  const sendJson = translateWord(req.body.word);
+  console.log("The word to translate is: " + req.body.word);
+  console.log(sendJson);
+  res.send(sendJson);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
