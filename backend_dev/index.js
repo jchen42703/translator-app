a821629a5820d72a5903e6dc0a668ec116b70035
Benson <36316652+Jinb2@users.noreply.github.com>
2021-05-02 00:18:27 -0400

    Update index.js
    - moved translateWord function
    - changed var to const

--- backend_dev/index.js
+++ backend_dev/index.js
@@ -3,28 +3,8 @@ const app = express();
 
 app.use(express.json());
 
-// read in the filenode
-const cedict = require("./cedict.json");
-
-/**
- * 
- * @param {String} word that you want to get information for 
- * @returns 
- */
-function translateWord(word) {
-  for (var i = 0; i < cedict.length; i++) {
-    if (word == cedict[i].traditional || word == cedict[i].simplfied) {
-      // console.log("The word found is" + word);
-      var translatedJson = { "english": cedict[i].english, "pinyin": cedict[i].pinyin };
-      break;
-    }
-    else {
-      var translatedJson = { "english": "Not found.", "pinyin": "Not found." };
-    }
-
-  }
-  return translatedJson;
-}
+// import our translate word function
+const translateWord = require("./translate");
 
 app.post("/translate/", (req, res) => {
   console.log("You are using the dev api...");
@@ -39,7 +19,7 @@ app.post("/translate/", (req, res) => {
 
 // return translated word
 app.post("/translateWord/", (req, res) => {
-  var sendJson = translateWord(req.body.word);
+  const sendJson = translateWord(req.body.word);
   console.log("The word to translate is: " + req.body.word);
   console.log(sendJson);
   res.send(sendJson);
@@ -48,11 +28,3 @@ app.post("/translateWord/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-
-
-
-
-
-
-
