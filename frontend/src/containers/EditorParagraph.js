30a7b65d3501cb0b4766c89b4629815bf7c8fb1b
jchen42703 <jchen42703@gmail.com>
2021-05-08 23:39:12 -0400

    Updated with clarification on EditorBlock v. EditorParagraph

--- frontend/src/containers/EditorParagraph.js
+++ frontend/src/containers/EditorParagraph.js
@@ -3,7 +3,8 @@ import { EditorBlock } from "draft-js";
 import "../styles/css/styles.css";
 
 /**
- * Represents a paragraph in the editor. Each paragraph is separated by a single new line.
+ * Represents a paragraph in the editor with the gutter (fold buttons). Each paragraph is separated by a single new line.
+ * Note: EditorBlock is contains the true text of the paragraph.
  */
 class EditorParagraph extends React.Component {
   render() {
