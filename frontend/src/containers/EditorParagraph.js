a5f7c174fa15bca3eeaf4c3fab679fc075945ba3
jchen42703 <jchen42703@gmail.com>
2021-05-09 23:04:20 -0400

    FoldButton and EditorBlock moved to the same line (no longer contain line numbers)

--- frontend/src/containers/EditorParagraph.js
+++ frontend/src/containers/EditorParagraph.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { EditorBlock } from "draft-js";
+import { FoldButton } from "./FoldBlockButton";
 import "../styles/css/styles.css";
 
 /**
@@ -8,17 +9,10 @@ import "../styles/css/styles.css";
  */
 class EditorParagraph extends React.Component {
   render() {
-    const { block, contentState } = this.props;
-    const lineNumber =
-      contentState
-        .getBlockMap()
-        .toList()
-        .findIndex((item) => item.key === block.key) + 1;
     return (
-      <div className="line" data-line-number={lineNumber}>
-        <div className="line-text">
-          <EditorBlock {...this.props} />
-        </div>
+      <div className="block">
+        <FoldButton></FoldButton>
+        <EditorBlock {...this.props} />
       </div>
     );
   }
