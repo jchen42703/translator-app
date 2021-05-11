553dccad51d98578af17b672bae77308b9d27a4f
jchen42703 <jchen42703@gmail.com>
2021-05-10 23:42:38 -0400

    Fixed bug where you could type in the rotate-button-wrapper and button

--- frontend/src/containers/FoldBlockButton.js
+++ frontend/src/containers/FoldBlockButton.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/css/styles.css";
 
 /**
  * Lies in the left gutter of the editor and display. When clicked,
  * it folds the paragraphs corresponding to its id in the editor and display.
+ *
+ * When the paragraph is collapsed, you cannot edit the contents of the corresponding paragraph.
  * @param {*} props should contain the key corresponding to the paragraph number
  */
 export const FoldButton = (props) => {
@@ -12,7 +14,7 @@ export const FoldButton = (props) => {
   };
 
   return (
-    <div className="rotate-button-wrapper">
+    <div className="rotate-button-wrapper" contentEditable={false}>
       <button
         className={props.collapse ? "button-fold" : "button-no-fold"}
         onClick={handleClick}
