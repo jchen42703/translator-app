574ca639611888e1a791e7661dd5ab00c5e2eb55
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-29 10:33:51 -0400

    Created a separate view for the translator page

---
+++ frontend/src/views/TranslationPage.js
@@ -0,0 +1,13 @@
+import Translator from "../containers/Translator";
+import "../styles/css/styles.css";
+
+const TranslationPage = () => {
+  return (
+    <div>
+      <h1 className="center-basic">Basic UI for Translation (Dev)</h1>
+      <Translator></Translator>
+    </div>
+  );
+};
+
+export default TranslationPage;
