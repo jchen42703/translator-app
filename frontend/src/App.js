9ce5905cad11b27b1c6810398435f4d1ccaedb92
Joseph Chen <34604336+jchen42703@users.noreply.github.com>
2021-08-29 11:27:55 -0400

    Added the routing for a basic LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from "react-router-dom";
 import TranslationPage from "./views/TranslationPage";
+import LoginPage from "./views/LoginPage";
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Switch>
-          <Route path="/">
-            <TranslationPage></TranslationPage>
+          <Route exact path="/">
+            <Redirect to="/login"></Redirect>
+          </Route>
+          <Route path="/login">
+            <LoginPage></LoginPage>
           </Route>
           <Route path="/translate">
             <TranslationPage></TranslationPage>
